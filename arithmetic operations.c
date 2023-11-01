2b1929015694ba284a46cfd0b5b88d3fd26050aa
mayankkumar12345 <143435689+mayankkumar12345@users.noreply.github.com>
2023-11-01 23:29:58 +0530

    Update arithmetic operations.c

--- arithmetic operations.c
+++ arithmetic operations.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int a,b;
+float a,b;
 printf("Enter the two numbers:");
-scanf("%d %d",&a,&b);
+scanf("%f %f",&a,&b);
 int c=a+b;
-printf("the sum of the two numbers is %d",c);
+printf("the sum of the two numbers is %d\n",c);
 int d=a*b;
-printf("the multiplication of two numbers is %d",d);
+printf("the multiplication of two numbers is %d\n",d);
 int e=a-b;
-printf("the substraction of two numbers is %d",e);
+printf("the substraction of two numbers is %d\n",e);
 float f=a/b;
 printf("the division of two numbers is %f",f);	
 		return 0;
 }
+
